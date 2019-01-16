770bd8c3c41d76a359c9f72931617dccab5eca69
Ryan Thomson <29545379+r-thomson@users.noreply.github.com>
2019-01-16 13:29:53 -0500

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const gulp = require('gulp');
+const { src, dest, series, parallel } = require('gulp');
 const del = require('del');
 const htmlmin = require('gulp-htmlmin');
 const postcss = require('gulp-postcss');
@@ -6,33 +6,33 @@ const autoprefixer = require('autoprefixer');
 const csso = require('postcss-csso');
 const minify = require('gulp-minify');
 
-gulp.task('clear', function() {
-	return del('build/**/*');
-})
+const srcDir = './src/';
+const buildDir = './build/'
 
-const copyFiles = ['src/lib/*', 'src/img/*.+(png|jpg|svg)', 'src/favicon.png', 'src/apple-touch-icon.png'];
-gulp.task('copy', function() {
-	return gulp.src(copyFiles, {base: 'src/'})
-		.pipe(gulp.dest('build/'))
-});
+function clean() {
+	return del(buildDir+'**/*');
+}
 
-const htmlFiles = ['src/*.html'];
-gulp.task('html', function(){
-	return gulp.src(htmlFiles)
+const copyFiles = ['lib/*', 'img/*.+(png|jpg|svg)', 'favicon.png', 'apple-touch-icon.png'];
+function copy(callback) {
+	return src(copyFiles, {cwd: srcDir, base: srcDir})
+		.pipe(dest(buildDir))
+}
+
+function html(callback) {
+	return src('*.html', {cwd: srcDir})
 		.pipe(htmlmin({
 			collapseWhitespace: true,
 			removeComments: true,
 			minifyCSS: true,
 			minifyJS: true
 		}))
-		.pipe(gulp.dest('build/'))
-});
+		.pipe(dest(buildDir))
+}
 
-const cssFiles = ['src/*.css', '!src/*.min.css'];
-gulp.task('css', function() {
-	return gulp.src(cssFiles)
+function css(callback) {
+	return src(['*.css', '*.min.css'], {cwd: srcDir}) 
 		.pipe(postcss([
-			//cssvariables(),
 			autoprefixer({
 				cascade: true
 			}),
@@ -40,19 +40,17 @@ gulp.task('css', function() {
 				restructure: false
 			})
 		]))
-		.pipe(gulp.dest('build/'))
-});
+		.pipe(dest(buildDir))
+}
 
-const jsFiles = ['src/*.js', '!src/gulpfile.js'];
-gulp.task('js', function() {
-	return gulp.src(jsFiles)
+function js(callback) {
+	return src(['*.js', '!gulpfile.js'])
 		.pipe(minify({
-			ext: {
-				min: '.js'	
-			},
+			ext: { min: '.js' },
 			noSource: true
 		}))
-		.pipe(gulp.dest('build/'))
-});
+		.pipe(dest(buildDir))
+}
 
-gulp.task('default', gulp.series('clear', gulp.parallel('copy', 'html', 'css', 'js')));
+exports.build = series(clean, parallel(copy, html, css, js));
+exports.default = exports.build
