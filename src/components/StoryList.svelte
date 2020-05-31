eeddb86935cd956a837e520966ff58dd1566d021
Ryan Thomson <git@ryanthomson.net>
2020-05-31 13:08:37 -0400

    Add pagination to story list

--- src/components/StoryList.svelte
+++ src/components/StoryList.svelte
@@ -3,19 +3,33 @@ import { fetchStoryIDs, fetchStory } from '../hn-api.js';
 
 export let list;
 
-let stories = fetchStoryIDs(list)
+const pageNum = Number.parseInt(new URLSearchParams(window.location.search).get('page'), 10) || 1;
+const pageLength = 30;
+
+// Indices of first and last items on the current page
+const first = pageLength * (pageNum - 1);
+const last = first + pageLength;
+
+const stories = fetchStoryIDs(list)
+	.then(ids => ids.slice(first, last)) // Pagination
 	.then(ids => ids.map(id => fetchStory(id)))
-	.then(stories => Promise.all(stories))
+	.then(stories => Promise.all(stories));
 </script>
 
 {#await stories}
-Loading
+	Loading
 {:then stories}
-<ol>
-	{#each stories as story}
-		<li>{story.title}</li>
-	{/each}
-</ol>
+	<ol start={first + 1}>
+		{#each stories as story}
+			<li>{story.title}</li>
+		{/each}
+	</ol>
+	<div>
+		{#if pageNum > 1}
+			<a href="?page={pageNum - 1}">Previous Page</a>
+		{/if}
+		<a href="?page={pageNum + 1}">Next Page</a>
+	</div>
 {:catch error}
-<code>{error}</code>
+	<code>{error}</code>
 {/await}
