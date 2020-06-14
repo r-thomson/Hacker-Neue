e57c15e5045051e03d25bc03426ea0dc15f8b555
Ryan Thomson <git@ryanthomson.net>
2020-06-14 19:51:42 -0400

    Add localStorage-backed store and add preference stores

---
+++ src/preferences.js
@@ -0,0 +1,14 @@
+import { writable } from 'svelte/store';
+
+const persistentStore = (key, initialValue) => {
+	const storedValue = JSON.parse(localStorage.getItem(key));
+	const store = writable(storedValue !== null ? storedValue : initialValue);
+	store.subscribe(newValue => localStorage.setItem(key, JSON.stringify(newValue)));
+	return store;
+};
+
+export const theme = persistentStore('pref-theme', 'auto');
+export const pageWidth = persistentStore('pref-page-width', '2');
+export const counters = persistentStore('pref-show-counters', false);
+export const maxStories = persistentStore('pref-max-stories', 30);
+export const highlightThreshold = persistentStore('pref-highlight-threshold', 250);
