69344cef7143abfb2ce3cfc4d7552ef57fb350b7
Ryan Thomson <git@ryanthomson.net>
2023-02-19 16:31:02 -0500

    Set esbuild legalComments='eof'
    Something like a quarter of the bundle size is license comments
    from the Firebase libraries. This setting preserves the comments
    while de-duping them.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [svelte()],
 	server: {
 		port: 3000,
+	},
+	esbuild: {
+		legalComments: 'eof',
 	},
 });
