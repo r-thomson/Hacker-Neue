28849b98cc72604391db15a7cca57fe43a7a2224
Ryan Thomson <git@ryanthomson.net>
2023-02-19 16:58:11 -0500

    Convert lists.js to .ts

--- src/lists.ts
+++ src/lists.ts
@@ -1,37 +1,38 @@
+import type { ComponentConstructorOptions } from 'svelte';
 import StoryList from './components/StoryList.svelte';
 
 export class TopStories extends StoryList {
-	constructor(options) {
+	constructor(options: ComponentConstructorOptions) {
 		super({ ...options, props: { list: 'top' } });
 	}
 }
 
 export class NewStories extends StoryList {
-	constructor(options) {
+	constructor(options: ComponentConstructorOptions) {
 		super({ ...options, props: { list: 'new' } });
 	}
 }
 
 export class BestStories extends StoryList {
-	constructor(options) {
+	constructor(options: ComponentConstructorOptions) {
 		super({ ...options, props: { list: 'best' } });
 	}
 }
 
 export class AskStories extends StoryList {
-	constructor(options) {
+	constructor(options: ComponentConstructorOptions) {
 		super({ ...options, props: { list: 'ask' } });
 	}
 }
 
 export class ShowStories extends StoryList {
-	constructor(options) {
+	constructor(options: ComponentConstructorOptions) {
 		super({ ...options, props: { list: 'show' } });
 	}
 }
 
 export class JobStories extends StoryList {
-	constructor(options) {
+	constructor(options: ComponentConstructorOptions) {
 		super({ ...options, props: { list: 'job' } });
 	}
 }
