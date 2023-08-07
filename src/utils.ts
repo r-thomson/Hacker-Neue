e70939ce053fab705d053db1fffac531d30603d9
Ryan Thomson <git@ryanthomson.net>
2023-08-07 08:27:37 -0400

    Add WeakValueMap to utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 import { derived, writable, type Readable, type Updater, type Writable } from 'svelte/store';
 
 /**
- * A derived store that updates after a delay since the latest change
+ * A derived store that updates after a delay since the latest change.
  * @param store Input store
  * @param delay Minimum delay in milliseconds
  * @param initialValue Initial value of the store
@@ -74,4 +74,38 @@ export function persistedStore<T>(
 			saveValue(newValue);
 		},
 	};
+}
+
+/**
+ * A Map that holds a weak reference to its values.
+ */
+export class WeakValueMap<K, V extends object> {
+	constructor(iterable?: Iterable<[K, V]> | null) {
+		const entries = iterable?.[Symbol.iterator]
+			? Array.from(iterable).map(([k, v]) => [k, new WeakRef(v)] as const)
+			: undefined;
+		this.#innerMap = new Map(entries);
+	}
+
+	#innerMap: Map<K, WeakRef<V>>;
+
+	get(key: K): V | undefined {
+		return this.#innerMap.get(key)?.deref();
+	}
+
+	has(key: K): boolean {
+		return this.get(key) !== undefined;
+	}
+
+	set(key: K, value: V): this {
+		this.#innerMap.set(key, new WeakRef(value));
+		return this;
+	}
+
+	delete(key: K): boolean {
+		// Map.delete returns a boolean so we need to retrieve before deleting
+		const existed = this.has(key);
+		this.#innerMap.delete(key);
+		return existed;
+	}
 }
