b8c8ef302751209ea31e400b5d1313aa29b43e82
Ryan Thomson <git@ryanthomson.net>
2024-03-10 13:03:51 -0400

    Use Intl for date formatting

--- src/components/Timestamp.svelte
+++ src/components/Timestamp.svelte
@@ -1,16 +1,27 @@
 <script lang="ts">
-	import { format, formatDistanceToNowStrict, formatISO } from 'date-fns';
+	import { differenceInDays, formatISO, intlFormat, intlFormatDistance } from 'date-fns';
 
 	export let date: Date;
+
+	function formatForDisplay(date: Date) {
+		const daysAgo = differenceInDays(date, Date.now()) * -1;
+		return daysAgo >= 5
+			? intlFormat(date, { dateStyle: 'medium' })
+			: intlFormatDistance(date, Date.now(), { numeric: 'always' });
+	}
+
+	function formatForTitle(date: Date) {
+		return intlFormat(date, { dateStyle: 'long', timeStyle: 'short' });
+	}
 </script>
 
-<time datetime={formatISO(date)} title={format(date, 'PP p')}>
-	{formatDistanceToNowStrict(date, { addSuffix: true })}
+<time datetime={formatISO(date)} title={formatForTitle(date)}>
+	{formatForDisplay(date)}
 </time>
 
 <style>
 	@media (hover: hover) {
-		time:hover {
+		time[title]:hover {
 			text-decoration-line: underline;
 			text-decoration-style: dotted;
 		}
