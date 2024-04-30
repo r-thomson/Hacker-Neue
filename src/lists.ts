6f5c8aad86c0e0a7c603fe59d23726acd03fea03
Ryan Thomson <git@ryanthomson.net>
2024-04-30 18:53:49 -0400

    Fix Svelte 5 incompatibilities

--- src/lists.ts
+++ src/lists.ts
@@ -1,38 +1,29 @@
-import type { ComponentConstructorOptions } from 'svelte';
-import StoryList from './components/StoryList.svelte';
+import type { ComponentType, ComponentProps, SvelteComponent } from 'svelte';
+import _StoryList from './components/StoryList.svelte';
 
-export class TopStories extends StoryList {
-	constructor(options: ComponentConstructorOptions) {
-		super({ ...options, props: { list: 'top' } });
-	}
-}
+// work around some typing limitations in svelte 5
 
-export class NewStories extends StoryList {
-	constructor(options: ComponentConstructorOptions) {
-		super({ ...options, props: { list: 'new' } });
-	}
-}
+type NewComponentType<T extends SvelteComponent = SvelteComponent> = (
+	anchor: Node,
+	props: ComponentProps<T>,
+) => T;
 
-export class BestStories extends StoryList {
-	constructor(options: ComponentConstructorOptions) {
-		super({ ...options, props: { list: 'best' } });
-	}
-}
+const StoryList: NewComponentType = _StoryList as any;
 
-export class AskStories extends StoryList {
-	constructor(options: ComponentConstructorOptions) {
-		super({ ...options, props: { list: 'ask' } });
-	}
-}
+export const TopStories = ((anchor, props) =>
+	StoryList(anchor, { ...props, list: 'top' })) as typeof StoryList as any as ComponentType;
 
-export class ShowStories extends StoryList {
-	constructor(options: ComponentConstructorOptions) {
-		super({ ...options, props: { list: 'show' } });
-	}
-}
+export const NewStories = ((anchor, props) =>
+	StoryList(anchor, { ...props, list: 'new' })) as typeof StoryList as any as ComponentType;
 
-export class JobStories extends StoryList {
-	constructor(options: ComponentConstructorOptions) {
-		super({ ...options, props: { list: 'job' } });
-	}
-}
+export const BestStories = ((anchor, props) =>
+	StoryList(anchor, { ...props, list: 'best' })) as typeof StoryList as any as ComponentType;
+
+export const AskStories = ((anchor, props) =>
+	StoryList(anchor, { ...props, list: 'ask' })) as typeof StoryList as any as ComponentType;
+
+export const ShowStories = ((anchor, props) =>
+	StoryList(anchor, { ...props, list: 'show' })) as typeof StoryList as any as ComponentType;
+
+export const JobStories = ((anchor, props) =>
+	StoryList(anchor, { ...props, list: 'job' })) as typeof StoryList as any as ComponentType;
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
+import { mount } from 'svelte';
 import App from './App.svelte';
 import './global.css';
 
-const app = new App({
+const app = mount(App, {
 	target: document.getElementById('app')!,
 });
 
