7969cd643c7bd2a0218a2e1d04baf14f0b868ffb
Ryan Thomson <git@ryanthomson.net>
2026-01-16 18:29:20 -0500

    Restore Hacker News API client

---
+++ src/lib/hacker-news/algolia.ts
@@ -0,0 +1,112 @@
+import type { DeadHNItem, DeletedHNItem, HNItem } from './types';
+
+interface SearchOptions {
+	ordering?: 'popularity' | 'date';
+	tags?: Tag[];
+	numericFilters?: string;
+	page?: number;
+	hitsPerPage?: number;
+}
+
+type Tag =
+	| 'story'
+	| 'comment'
+	| 'poll'
+	| 'pollopt'
+	| 'show_hn'
+	| 'ask_hn'
+	| 'front_page'
+	| `author_${string}`
+	| `story_${string}`;
+
+type SearchResult = Exclude<HNItem, DeletedHNItem | DeadHNItem>;
+
+/**
+ * Perform a search with the public Algolia HN API.
+ * @param query - Text to search for
+ * @param options - Additional search options
+ */
+export async function search(query: string, options: SearchOptions = {}): Promise<SearchResult[]> {
+	const url = new URL('https://hn.algolia.com/api/v1');
+	url.pathname += options.ordering === 'date' ? '/search_by_date' : '/search';
+
+	url.searchParams.set('query', query);
+	if (options.tags) url.searchParams.set('tags', stringifyArray(options.tags));
+	if (options.numericFilters) url.searchParams.set('numericFilters', options.numericFilters);
+	if (options.page) url.searchParams.set('page', options.page.toString());
+	if (options.hitsPerPage) url.searchParams.set('hitsPerPage', options.hitsPerPage.toString());
+
+	const response = await fetch(url);
+	if (response.status !== 200) throw Error(response.statusText);
+
+	const { hits } = await response.json();
+	return hits.map((hit: any) => searchItemToHNItem(hit));
+}
+
+function stringifyArray(array: Array<string | string[]>): string {
+	return array.map((value) => (Array.isArray(value) ? `(${value.join(',')})` : value)).join(',');
+}
+
+function searchItemToHNItem(searchHit: any): SearchResult {
+	// Working off the assumption that the item's "type" is always the first tag
+	const typeTags = new Set(['story', 'comment', 'poll', 'pollopt', 'job']);
+	const type: string | undefined = searchHit._tags.find((tag: string) => typeTags.has(tag));
+
+	switch (type) {
+		case 'story':
+			return {
+				id: searchHit.objectID,
+				type: 'story',
+				time: searchHit.created_at_i,
+				title: searchHit.title,
+				url: searchHit.url,
+				text: searchHit.story_text,
+				by: searchHit.author,
+				score: searchHit.points,
+				descendants: searchHit.num_comments,
+			};
+		case 'comment':
+			return {
+				id: searchHit.objectID,
+				type: 'comment',
+				time: searchHit.created_at_i,
+				text: searchHit.comment_text,
+				by: searchHit.author,
+				parent: searchHit.parent_id,
+			};
+		case 'poll':
+			return {
+				id: searchHit.objectID,
+				type: 'poll',
+				time: searchHit.created_at_i,
+				title: searchHit.title,
+				by: searchHit.author,
+				score: searchHit.points,
+				parts: [], // Not supported :(
+				descendants: searchHit.num_comments,
+			};
+		case 'pollopt':
+			return {
+				id: searchHit.objectID,
+				type: 'pollopt',
+				time: searchHit.created_at_i,
+				by: searchHit.author,
+				text: searchHit.story_text,
+				poll: searchHit.parent_id,
+				score: searchHit.points,
+			};
+		case 'job':
+			return {
+				id: searchHit.objectID,
+				type: 'job',
+				time: searchHit.created_at_i,
+				title: searchHit.title,
+				url: searchHit.url,
+				text: searchHit.story_text,
+				by: searchHit.author,
+				score: searchHit.points,
+			};
+		default:
+			throw Error(`Unexpected type "${type}"`);
+	}
+}
---
+++ src/lib/hacker-news/api.ts
@@ -0,0 +1,93 @@
+import type { HNItem, HNList, HNUser } from './types';
+
+export namespace symbols {
+	export const resolvedKids: unique symbol = Symbol('resolvedKids');
+	export const rootItem: unique symbol = Symbol('rootItem');
+}
+
+/**
+ * Retrieves an array of story IDs for a given story list.
+ * @param list - Name of the story list (top, new, ask, etc.)
+ */
+export async function fetchStoryIds(list: HNList): Promise<number[]> {
+	const { get, child, firebaseDbRef } = await import('./firebase');
+
+	const snapshot = await get(child(firebaseDbRef, `${list}stories`));
+	return snapshot.val();
+}
+
+/**
+ * Retrieves the item with the given ID. Returns `null` if it doesn't exist.
+ * @param id - The item's ID
+ */
+export async function fetchItem(id: number): Promise<HNItem | null> {
+	const { get, child, firebaseDbRef } = await import('./firebase');
+
+	const snapshot = await get(child(firebaseDbRef, `item/${id}`));
+	const item = snapshot.val();
+
+	// Sometimes the API returns a comment with "* * *" as the text. It's not clear why
+	// this happens. These comments aren't visible on Hacker News, so we'll treat them
+	// as deleted for now.
+	if (item?.text === '* * *') {
+		item.deleted = true;
+	}
+
+	return item;
+}
+
+/**
+ * Retrieves the user with the given ID. Returns `null` if it doesn't exist.
+ * @param id - The user's ID
+ */
+export async function fetchUser(id: string): Promise<HNUser | null> {
+	const { get, child, firebaseDbRef } = await import('./firebase');
+
+	const snapshot = await get(child(firebaseDbRef, `user/${id}`));
+	return snapshot.val();
+}
+
+export type FetchedKids = {
+	[symbols.resolvedKids]: (HNItem & FetchedKids)[];
+	[symbols.rootItem]: HNItem;
+};
+
+/**
+ * Recursively retrieves the kid items of a given item.
+ * @param parent - The parent item (should contain a `kids` property)
+ * @param onFetchItem - Called whenever an item is fetched
+ */
+export async function fetchKids(
+	parent: HNItem,
+	onFetchItem?: () => void,
+	_depth = 1,
+): Promise<(HNItem & FetchedKids)[]> {
+	if (!('kids' in parent) || !parent.kids) return [];
+
+	const kidsPromises = parent.kids.map(async (itemId) => {
+		const item = await fetchItem(itemId);
+
+		if (!item) return null;
+
+		// Hack to support tracking progress on the items page
+		if (!item.deleted && !item.dead) {
+			onFetchItem?.();
+		}
+
+		// We need to assign @@rootItem before recursively calling fetchKids()
+		// Admittedly I am not sure how to represent this well in TypeScript
+		const itemWithRoot = Object.assign(item, {
+			[symbols.rootItem]: symbols.rootItem in parent ? parent[symbols.rootItem] : parent,
+		} as Pick<FetchedKids, typeof symbols.rootItem>);
+
+		return Object.assign(itemWithRoot, {
+			[symbols.resolvedKids]: await fetchKids(itemWithRoot, onFetchItem, _depth + 1),
+		} as Pick<FetchedKids, typeof symbols.resolvedKids>);
+	});
+
+	const kids = await Promise.all(kidsPromises);
+	return kids.filter((item): item is NonNullable<typeof item> => !!item);
+}
+
+// Sourced from https://github.com/minimaxir/hacker-news-undocumented#moderators
+export const modNames = Object.freeze(new Set(['dang', 'sctb', 'tomhow']));
