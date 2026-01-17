7cc05cef4f0af723fe44067077e896c210189b93
Ryan Thomson <git@ryanthomson.net>
2026-01-16 23:32:47 -0500

    Clean up vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,64 @@
-import { defineConfig } from 'vitest/config';
-import { playwright } from '@vitest/browser-playwright';
+/// <reference types="vitest/config" />
 import { sveltekit } from '@sveltejs/kit/vite';
+import autoprefixer from 'autoprefixer';
+import type { PluginVisualizerOptions } from 'rollup-plugin-visualizer';
+import { defineConfig } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()],
+export default defineConfig(({ command, mode }) => {
+	const nodeMajor = +process.versions.node.split('.')[0];
+	const analyze = command === 'build' && mode === 'analyze';
 
-	test: {
-		expect: { requireAssertions: true },
-
-		projects: [
-			{
-				extends: './vite.config.ts',
-
-				test: {
-					name: 'client',
-
-					browser: {
-						enabled: true,
-						provider: playwright(),
-						instances: [{ browser: 'chromium', headless: true }],
-					},
-
-					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
-					exclude: ['src/lib/server/**'],
-				},
-			},
-
-			{
-				extends: './vite.config.ts',
-
-				test: {
-					name: 'server',
-					environment: 'node',
-					include: ['src/**/*.{test,spec}.{js,ts}'],
-					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}'],
-				},
-			},
+	return defineConfig({
+		plugins: [
+			sveltekit(),
+			analyze &&
+				visualizer({
+					template: 'sunburst',
+				}),
 		],
-	},
+		resolve: process.env.VITEST ? { conditions: ['browser'] } : undefined,
+		css: {
+			postcss: {
+				plugins: [autoprefixer()],
+			},
+		},
+		server: {
+			port: 3000,
+		},
+		esbuild: {
+			legalComments: 'eof',
+		},
+		test: {
+			environment: 'happy-dom',
+			execArgv: nodeMajor >= 25 ? ['--no-experimental-webstorage'] : [],
+			restoreMocks: true,
+
+			projects: [
+				{
+					extends: true,
+					test: {
+						name: 'client',
+						environment: 'happy-dom',
+						include: ['src/**/*.svelte.test.{js,ts}'],
+						exclude: ['src/lib/server/**'],
+					},
+				},
+				{
+					extends: true,
+					test: {
+						name: 'server',
+						environment: 'node',
+						include: ['src/**/*.test.{js,ts}'],
+						exclude: ['src/**/*.svelte.test.{js,ts}'],
+					},
+				},
+			],
+		},
+	});
 });
+
+async function visualizer(options: PluginVisualizerOptions) {
+	// Async import so the dependency is optional
+	const { visualizer } = await import('rollup-plugin-visualizer');
+	return visualizer(options);
+}
